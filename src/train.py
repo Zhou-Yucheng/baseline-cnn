b7cf4615e79f2bd7231194eadb6fe12ab663bca7
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-20 20:09:44 +0800

    major update& fix

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@
 
 import os, csv, socket, time, shutil, pathlib
 from datetime import datetime
+from glob import glob
 
 import argparse
 import matplotlib.pyplot as plt
@@ -19,42 +20,51 @@ from torch.utils.tensorboard import SummaryWriter
 import data
 
 
-def plot_model_history(log_path, plot_loss=True):
+def plot_train_history(log_path=None, plot_loss=True):
+    if log_path == None:
+        csvs=glob(f'{output_dir}/csvs/*.csv')
+        csvs.sort(key=os.path.getctime)
+        log_path=csvs[-1]
+    log('plot model training history:', log_path)
+
+    # Epoch, Train Loss, Train Acc, Valid Loss, Valid Acc, LR
     train_loss, train_acc, val_loss, val_acc = [], [], [], []
     with open(log_path) as f:
         f_csv = csv.reader(f)
         headers = next(f_csv)
         for row in f_csv:
-            if (not row) or row[0] == 'Epoch':
+            if (not row) or row[0] == 'Epoch' or row[0][0] == '#':
                 continue
-            if '#' in row[0]:
-                break
             row = list(map(float, row))
             train_loss.append(row[1])
             train_acc.append(row[2])
             val_loss.append(row[3])
             val_acc.append(row[4])
 
-    plt.plot(train_acc, linewidth=0.5)
+    plt.figure()
+    plt.plot(train_acc) #, linewidth=0.5
     plt.plot(val_acc)
     plt.title('Model Accuracy')
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Valid'], loc='upper left')
     plt.show()
+    plt.savefig(f'{output_dir}/training-curve-acc.jpg')
 
     if plot_loss:
         plt.figure()
-        plt.plot(train_loss, linewidth=0.5)
+        plt.plot(train_loss)
         plt.plot(val_loss)
         plt.title('Model Loss')
         plt.ylabel('Loss')
         plt.xlabel('Epoch')
-        plt.legend(['Train', 'Valid'], loc='upper left')
+        plt.legend(['Train', 'Valid'], loc='upper right')
         plt.show()
+        plt.savefig(f'{output_dir}/training-curve-loss.jpg')
 
 
-def plot_model_CM(model, valid_loader):
+def plot_confusion_matrix(model, valid_loader):
+    log('plot confusion matrix ...')
     model.eval()
     CM = torch.zeros((n_classes, n_classes))
     n_correct = 0
@@ -76,15 +86,17 @@ def plot_model_CM(model, valid_loader):
                     CM[preds[i]][labels[i]] += 1
 
     df_cm = pd.DataFrame(CM.numpy(), range(n_classes), range(n_classes))
-    # plt.figure(figsize=(10,7))
-    # sn.set(font_scale=1.4)  # for label size
-    sn.heatmap(df_cm, annot=True, annot_kws={"size": 12}, cmap='Greys')  # font size
+    plt.figure()
+    sn.heatmap(df_cm, annot=False, annot_kws={"size": 12}, cmap='Greys')  # font size
     plt.show()
-    log('\nThe plotted confusion matrix is based on {} random valid sample'.format(n_valid))
-    log('\tValid Acc = {:4f} ({}/{})'.format((n_correct + 0.0) / n_valid, n_correct, n_valid))
+    plt.savefig(f'{output_dir}/confusion-matrix.jpg')
+    log('confusion matrix is based on {} random valid sample'.format(n_valid))
+    log('val acc = {:4f} ({}/{})'.format((n_correct + 0.0) / n_valid, n_correct, n_valid))
 
 
-def plot_tsne(model, valid_loader):
+def plot_tsne_vis(model, valid_loader):
+    """model: should has attr feature_map"""
+    log('plot t-SNE ...')
     model.eval()
     epoch_plot = 9
     time_start = time.time()
@@ -99,14 +111,12 @@ def plot_tsne(model, valid_loader):
                 outputs = model(inputs)
                 _, preds = torch.max(outputs, 1)
 
-                x = torch.cat((x, model.feature_map),
-                              0) if x is not None else model.feature_map
+                x = torch.cat((x, model.feature_map),0) if x is not None else model.feature_map
                 y = torch.cat((y, preds), 0) if y is not None else preds
         x = x.squeeze().cpu().numpy()
         y = y.cpu().numpy()
 
     # log('Time for retrieving feature map:', time.time() - time_start)
-
     # x = np.load('x.npy')
     # y = np.load('y.npy')
 
@@ -122,14 +132,18 @@ def plot_tsne(model, valid_loader):
             plt.title(title)
         plt.show()
         # plt.savefig('../photos/{} t-SNE features visualization')
+        plt.savefig(f'{output_dir}/t-SNE.jpg')
 
     tsne = manifold.TSNE(n_components=2, init='pca')
     x_tsne = tsne.fit_transform(x)
     plot_embedding(x_tsne)
-    log('Time for TSNE:', time.time() - time_start)
+    log('time for t-SNE:', time.time() - time_start)
 
 
-def vis_conv(model, valid_loader):
+def plot_conv_vis(model, valid_loader):
+    """model: should has attr feature_map"""
+    log('plot conv layer visualization ...')
+
     model.eval()
     time_start = time.time()
     # log('Start')
@@ -176,12 +190,11 @@ def vis_conv(model, valid_loader):
 
     i_batch = 1
     n_square = 8
-    plot_conv_feature_map(x0, i_batch, n_square)
-    plot_conv_feature_map(x1, i_batch, n_square)
-    plot_conv_feature_map(x2, i_batch, n_square)
-    plot_conv_feature_map(x3, i_batch, n_square)
-    plot_conv_feature_map(x4, i_batch, n_square)
-    log('Time for visualize conv layer:', time.time() - time_start)
+    for i, x in enumerate([x0,x1,x2,x3,x4]):
+        plot_conv_feature_map(x, i_batch, n_square)
+        plt.savefig(f'{output_dir}/conv-vis-{i}.jpg')
+        
+    log('time for visualizing conv layer:', time.time() - time_start)
 
 
 def train():
@@ -189,7 +202,8 @@ def train():
     total_loss = 0.0
     total_correct = 0
 
-    for inputs, labels in train_loader:
+    for i, (inputs, labels) in enumerate(train_loader):
+        print(f'\rtraining {i+1}/{len(train_loader)} ...', end='')
         inputs = inputs.to(device)
         labels = labels.to(device)
 
@@ -203,6 +217,7 @@ def train():
         total_loss += loss.item() * inputs.size(0)
         total_correct += torch.sum(predictions == labels.data)
 
+    print('\r',end='')
     epoch_loss = total_loss / len(train_loader.dataset)
     epoch_acc = total_correct.double() / len(train_loader.dataset)
     return epoch_loss, epoch_acc.item()
@@ -235,7 +250,7 @@ def log_and_save():
     if val_acc > best_val_acc:
         best_val_acc = val_acc
         best_epoch = epoch
-        torch.save(model.state_dict(), f'./models/{args.model_name}_final.pth')
+        torch.save(model.state_dict(), last_model_path)
 
     # ===Writer
     fwriter.write(f'{epoch}, {train_loss}, {train_acc}, {val_loss}, {val_acc}, {lr}\n')
@@ -246,8 +261,8 @@ def log_and_save():
 
     # ===Print result, Rename (copy) saved model, Close writer
     if epoch == args.epochs:
-        log(f'Best Valid Acc: {best_val_acc:.4f}, at epoch: {best_epoch}')
-        shutil.copy(f'./models/{args.model_name}_final.pth', f'./models/{model_fullname}-ep{best_epoch}-acc{best_val_acc*100:.1f}.pth')
+        log(f'best val acc: {best_val_acc:.4f}, at epoch: {best_epoch}')
+        shutil.copy(last_model_path, f'{output_dir}/{model_fullname}-ep{best_epoch}-acc{best_val_acc*100:.1f}.pth')
         fwriter.close()
         swriter.close()
 
@@ -275,13 +290,13 @@ def get_args():
     parser = argparse.ArgumentParser(description='Baseline CNN')
 
     parser.add_argument('-m', '--model_name', type=str, default='resnet18', help='model name in torchvision')
-    parser.add_argument('-e', '--epochs', type=int, default=10, help='num of training epochs')
+    parser.add_argument('-e', '--epochs', type=int, default=20, help='num of training epochs')
     parser.add_argument('-b', '--batch_size', type=int, default=32, help='batch size')
     parser.add_argument('-l', '--lr', type=float, default=1e-3, help='learning rate')
     parser.add_argument('-c', '--cuda', type=str, default='', help='cuda visible device id')
     parser.add_argument('-r', '--resume', action='store_true', help='resume training')
     parser.add_argument('-g', '--gamma', type=float, default=0.1, help='lr gamma')
-    parser.add_argument('-w', '--warmup_epochs', type=int, default=5, help='lr warmup epochs')
+    parser.add_argument('-w', '--warmup_epochs', type=int, default=10, help='lr warmup epochs')
     parser.add_argument('-s', '--step', type=int, default=1000, help='lr decrease step')
     parser.add_argument('--step2', type=int, default=2000, help='lr decrease step')
     parser.add_argument('--eval_only', action='store_true', help='eval model and exit')
@@ -298,22 +313,30 @@ def get_args():
     return args_
 
 
-def log(msg, end='\n'):
-    log_path='./logs/log.log'
+def log(*msgs):
+    log_path=f'{output_dir}/log.log'
+    msg = ' '.join(msgs)
     dt_now=datetime.now().strftime('%Y/%m/%d %H:%M:%S')
     with open(log_path, 'a+') as fp:
-        fp.write(f'[{dt_now}] {msg}{end}')
-    print(f'[{dt_now}] {msg}', end=end)
+        fp.write(f'[{dt_now}] {msg}\n')
+    print(f'[{dt_now}] {msg}')
+
+
+def load_last_model():
+    model.load_state_dict(torch.load(last_model_path))
+    log(f'load model: {last_model_path}')
 
 
 if __name__ == '__main__':
-    args = get_args()
     start_time = time.time()
-    n_classes = 20
+    args = get_args()
+    n_classes = 67
     input_size = 224
+    output_dir = './output' if args.cuda=='0' else './output'+args.cuda
+    last_model_path = f'{output_dir}/{args.model_name}_final.pth'
 
     train_loader, valid_loader = data.get_image_folder_data_loader(
-        data_dir='../data/ClipArt20', input_size=input_size, batch_size=args.batch_size)
+        data_dir='../data/MIT67', input_size=input_size, batch_size=args.batch_size)
     
     model = get_model(args.model_name, n_classes)
     os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda
@@ -328,25 +351,25 @@ if __name__ == '__main__':
     _r = '-r' if args.resume else ''
     _s = 's' if args.step < args.epochs else ''
     model_fullname = f"{args.model_name}-bs{args.batch_size:02d}-lr{_s}{_lr}{_r}".replace('e-0', 'e-')
-
-    if args.eval_only:
-        model.load_state_dict(torch.load(f'./models/{args.model_name}_final.pth'))
-        plot_model_history(f'./logs/{model_fullname}.csv')
-        plot_tsne(model, valid_loader)
-        vis_conv(model, valid_loader)
-        plot_model_CM(model, valid_loader)
-    if args.resume:
-        model.load_state_dict(torch.load(f'./models/{args.model_name}_final.pth'))
     
-    pathlib.Path('./logs/csvs').mkdir(parents=True, exist_ok=True)
-    pathlib.Path('./logs/runs').mkdir(exist_ok=True)
-    pathlib.Path('./models').mkdir(exist_ok=True)
+    if args.eval_only:
+        load_last_model()
+        plot_train_history()
+        plot_confusion_matrix()
+        exit()
+    if args.resume:
+        load_last_model()
+    pathlib.Path(output_dir).mkdir(exist_ok=True)
+    pathlib.Path(f'{output_dir}/csvs').mkdir(exist_ok=True)
+    pathlib.Path(f'{output_dir}/runs').mkdir(exist_ok=True)
     dt_now = datetime.now().strftime('%m%d-%H%M')
-    swriter = SummaryWriter(log_dir=f'./logs/runs/{dt_now} {model_fullname}')
-    fwriter = open(f'./logs/csvs/{dt_now} {model_fullname}.csv', 'a+')
+    swriter = SummaryWriter(log_dir=f'{output_dir}/runs/{dt_now} {model_fullname}')
+    fwriter = open(f'{output_dir}/csvs/{dt_now} {model_fullname}.csv', 'w')
     fwriter.write('Epoch, Train Loss, Train Acc, Valid Loss, Valid Acc, LR\n')
-    log('Args: ' + str(args))
-    log(f'Model full name: {model_fullname}')
+    log('#'*100+'\n')
+    log('Args:', str(args))
+    log('Model full name:', model_fullname)
+
     best_val_acc = 0.0
     best_epoch = 0
     epoch = 1
@@ -358,8 +381,9 @@ if __name__ == '__main__':
             log_and_save()
     except KeyboardInterrupt:
         log('*KeyboardInterrupt, early stop')
-        args.epochs = epoch
-        log_and_save()
+        if epoch > 1:
+            args.epochs = epoch
+            log_and_save()
     
     t = time.time() - start_time
-    log(f'Training {epoch} epochs finished in {t/60:.2f} min, speed: {t/epoch:.1f} s/epoch')
+    log(f'train {epoch} epochs finished in {t/60:.1f} min, overall training speed: {t/epoch:.1f} s/epoch')
